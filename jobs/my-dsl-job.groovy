7360a51f6971e58e4e63d4d7ef68fc2ba1119e1c
apurva <apag1996jpr@gmail.com>
2025-02-22 19:05:02 +0530

    com

---
+++ jobs/my-dsl-job.groovy
@@ -0,0 +1,15 @@
+job('MyGeneratedJob') {
+    description('This job is generated via Job DSL from GitHub')
+
+    scm {
+        git('https://github.com/apurva0495/JenkinsPracticeArti.git', 'master')
+    }
+
+    triggers {
+        cron('H 12 * * 1-5') // Runs at 12 PM on weekdays
+    }
+
+    steps {
+        shell('echo "Hello, Jenkins! Running Job from DSL script"')
+    }
+}
