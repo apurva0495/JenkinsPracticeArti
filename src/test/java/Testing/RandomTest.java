298ff03d2f23ddf89a48ae04535e076ae190f7f0
apurva <apag1996jpr@gmail.com>
2025-03-03 19:52:42 +0530

    comsss

--- src/test/java/Testing/RandomTest.java
+++ src/test/java/Testing/RandomTest.java
@@ -1,12 +1,30 @@
 package Testing;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class RandomTest {
 
-	
+	public WebDriver driver;
 	@Test
-	public void testOne() {
-		System.out.println("Test One");
+	@Parameters("browser")
+	public void chromeB(String browser) {
+		if(browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			 driver = new ChromeDriver();
+			
+		}else if(browser.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			 driver = new FirefoxDriver();
+		}
+		driver.get("https://www.google.com/");
+		System.out.println(driver.getTitle());
 	}
+	
+	
 }
